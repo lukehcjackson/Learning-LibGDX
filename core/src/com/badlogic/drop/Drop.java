b0b77357193129935f82952eed8dcab37baf1e44
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-02-27 16:36:28 +0000

    Raindrop logic

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -8,9 +8,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class Drop extends ApplicationAdapter {
 	//Instantiate variables for our assets
@@ -19,10 +24,30 @@ public class Drop extends ApplicationAdapter {
 	private Sound dropSound;
 	private Music rainMusic;
 
+	//camera
 	private OrthographicCamera camera;
+	//spritebatch object
 	private SpriteBatch batch;
 
+	//bucket object
 	private Rectangle bucket;
+
+	//raindrop related variables
+	private Array<Rectangle> raindrops;
+	//use libgdx Array rather than Java array to reduce garbage collection needed (same for sets and hashmaps)
+
+	private long lastDropTime;
+	//time since we last spawned a raindrop
+	//stored in nanoseconds so use a long
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64); //random x-pos that the bucket can reach
+		raindrop.y = 480; //top of the screen
+		raindrop.width = 64; raindrop.height = 64;
+		raindrops.add(raindrop); //add raindrop object to raindrops array
+		lastDropTime = TimeUtils.nanoTime();
+	}
 
 	//CONSIDER create() LIKE SETUP() - RUNS ONCE, BEFORE render()
 	@Override
@@ -65,11 +90,23 @@ public class Drop extends ApplicationAdapter {
 		bucket.width = 64;
 		bucket.height = 64;
 		//LibGDX transformation origin is BOTTOM LEFT of the screen!!
+
+		//---------------------------------------------
+		//--------------RAINDROP SETUP-----------------
+		//---------------------------------------------
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+
 	}
 
 	//CONSIDER render() LIKE DRAW - RUNS ONCE PER FRAME
 	@Override
 	public void render() {
+		//---------------------------------------------
+		//--------------SETUP CAMERA-------------------
+		//---------------------------------------------
+
 		//render background colour (takes RGBA values from 0-1)
 		ScreenUtils.clear(0,0,0.2f,1);
 
@@ -81,19 +118,57 @@ public class Drop extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		//start a new sprite batch - allows OpenGL optimisations in batching
 		batch.begin();
+
 		//draw() takes a Texture and an x,y pos
+
+		//draw the bucket
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		//draw all raindrops
+		//for each raindrop in raindrops
+		for (Rectangle raindrop : raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+
 		//calling .end() submits all draw() calls at once
 		batch.end();
 		//rendering this way makes it much faster
 
-		//MOVEMENT LOGIC
+		//---------------------------------------------
+		//--------------RAINDROP LOGIC-----------------
+		//---------------------------------------------
+
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+			//spawn a new raindrop every 1000000000ns (1 second)
+			spawnRaindrop();
+		}
+
+		//make the raindrops fall
+		//for each raindrop in raindrops array
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();) {
+			Rectangle raindrop = iter.next();
+			//move down by 200 pixels
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			//if off the bottom of the screen, remove it from the array
+			if (raindrop.y + 64 < 0) {
+				iter.remove();
+			}
+			//if caught by a bucket
+			if (raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
+
+		//---------------------------------------------
+		//--------------MOVEMENT LOGIC-----------------
+		//---------------------------------------------
 
 		//center the button horizontally around the clicked position
 		//check is screen is pressed / mouse is pressed
 		if (Gdx.input.isTouched()) {
-			//VERY BAD!! TO INSTANTIATE A V3 IN RENDER()
+			//!!VERY BAD!! TO INSTANTIATE A new V3 (or any variable) IN RENDER()
 			//THIS IS LEAKING MEMORY - INSTANTIATE AS A CLASS VARIABLE THEN REUSE / REASSIGN IN RENDER()
+			//the garbage collector will remove all the old references to created V3()'s, but this is SLOW and hurts performance
 			Vector3 touchPos = new Vector3();
 			//get x,y position of click
 			//store as V3 to make it easy to map back into 2D using a camera un-projection
@@ -119,7 +194,9 @@ public class Drop extends ApplicationAdapter {
 			bucket.x += 200f * Gdx.graphics.getDeltaTime();
 		}
 
-		//COLLISION LOGIC
+		//---------------------------------------------
+		//--------------COLLISIONS---------------------
+		//---------------------------------------------
 
 		//make sure the bucket does not go off the sides of the screen
 		if (bucket.x < 0) bucket.x = 0;

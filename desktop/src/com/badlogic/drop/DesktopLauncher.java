7d6833819f392e68a82a4754df61be6bfa4c4df2
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-02-27 15:06:41 +0000

    Configure starter classes
    Added assets and a notes file. Configure starter classes to run an 800x480 window

--- desktop/src/com/badlogic/drop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -8,8 +8,11 @@ import com.badlogic.drop.Drop;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("drop");
+
 		new Lwjgl3Application(new Drop(), config);
 	}
 }
